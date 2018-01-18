c942a640ce18353a29bea55828d6e396aa00be47
rjarek <softart.poczta@gmail.com>
2018-01-18 18:28:28 +0100

    start

---
+++ app.js
@@ -0,0 +1,20 @@
+
+//uwaga tutaj ponzej jest klasa - czyli zapisa .przycisk
+$('.przycisk-subscribe').on('click', function(){
+
+
+    alert(1);
+   $('#emailField').addClass('animated bounceOutLeft');
+   
+   //co zrobic po skonczeniu animacji
+   //$('#yourElement').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
+
+   //uwage tutaj jest id czyli #
+   $('#emailField').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+
+        $('#emailField').removeClass('animated bounceOutLeft');
+
+
+   });
+
+});
